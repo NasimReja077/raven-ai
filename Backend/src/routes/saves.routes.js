e2ff9e881396db7059bb662da37633b7b3ffa515
Nasim <nasim.reja.077@gmail.com>
2026-04-04 19:34:59 +0530

    Add uploadFile, deleteSaveFile clustering router

--- Backend/src/routes/saves.routes.js
+++ Backend/src/routes/saves.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protect from "../middlewares/auth.middleware.js";
-// import { uploadFile } from "../middlewares/uploadMiddleware.js";
+import { uploadFile } from "../middlewares/uploadMiddleware.js";
 import {
      createSave, getAllSaves, getSaveById, 
      updateSave, deleteSave,getSaveToResurface, 
@@ -8,19 +8,21 @@ import {
      addHighlight, deleteHighlight, addToCollection, 
      removeFromCollection,
 } from "../controllers/saves.controller.js";
-// import { getSimilarItems } from "../controllers/clustering.controller.js";
+import { uploadSaveFile, deleteSaveFile } from "../controllers/fileUpload.controller.js";
+import { getSimilarItemsHandler } from "../controllers/clustering.controller.js";
 
 const router = express.Router();
 router.use(protect);
 
+router.post("/uploadFile", uploadFile.single("file"), uploadSaveFile);
+
 router.post("/", createSave);
-// router.post("/upload", uploadFile.single("file"), createSave);
 router.get("/", getAllSaves);
 router.get("/stats", getSaveStats);
 router.get("/resurface", getSaveToResurface);
 router.get("/:id", getSaveById);
 router.get("/:id/related", getRelatedSaves);
-// router.get("/:id/similar", getSimilarItems);       // clustering
+router.get("/:id/similar", getSimilarItemsHandler);       // clustering
 router.patch("/:id", updateSave);
 router.delete("/:id", deleteSave);
 router.post("/:id/reprocess", reprocessSave);
@@ -28,5 +30,7 @@ router.post("/:id/highlights", addHighlight);
 router.delete("/:id/highlights/:hId", deleteHighlight);
 router.post("/:id/collections", addToCollection);
 router.delete("/:id/collections/:collectionId", removeFromCollection);
+
+router.delete("/:id/file", deleteSaveFile);
 
 export default router;
